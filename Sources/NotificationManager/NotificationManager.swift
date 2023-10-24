80f2005cacfa7fc879e6fb8cbc0d22a2cadd34d1
BadWolf20 <Roman.Pichugin20@gmail.com>
2023-10-24 13:54:07 +0700

    Debug

--- Sources/NotificationManager/NotificationManager.swift
+++ Sources/NotificationManager/NotificationManager.swift
@@ -3,7 +3,7 @@
 
 import UserNotifications
 
-class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
+public class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
     public static let shared = NotificationManager()
 
     private let notificationCenter = UNUserNotificationCenter.current()
@@ -11,7 +11,7 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
     private override init() { }
 
     /// Запрашивает разрешение на отправку уведомлений пользователю.
-    func requestNotificationAccess() {
+    public func requestNotificationAccess() {
         notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
             guard granted else { return }
             self.notificationCenter.getNotificationSettings { (settings) in
@@ -24,7 +24,7 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
     /// Отправляет уведомление с указанным заголовком.
     ///
     /// - Parameter title: Заголовок уведомления
-    func sendNotification(withTitle title: String) {
+    public func sendNotification(withTitle title: String) {
         let content = UNMutableNotificationContent()
         content.title = title
         content.sound = UNNotificationSound.default
@@ -37,7 +37,7 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
     }
 
     /// Отображает уведомление внутри приложения.
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         if #available(iOS 14.0, *) {
             completionHandler([.sound, .banner])
         } else {
@@ -46,7 +46,7 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
     }
 
     /// Обрабатывает действия пользователя при нажатии на уведомление.
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    public func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         print(#function)
     }
 }
